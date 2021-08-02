2e1ac0a57577ed76c28d77ece63ba0d4a6d5fb64
AlexMc124 <alex.mcintosh124@gmail.com>
2021-08-02 19:39:52 +0100

    Commit-1-part7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SearchBar from "./searchbar";
+import AddItem from "./AddItem";
+import { useState } from "react";
+import ItemsDisplay from "./ItemsDisplay";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [filters, setFilters] = useState({});
+  const [data, setData] = useState({ items: [] });
+
+  const addItemToData = (item) => {
+    let items = data["items"];
+    item.id = items.length;
+    items.push(item);
+    setData({ items: items });
+    console.log(data);
+  };
+
+  const updateFilters = (searchParams) => {
+    setFilters(searchParams);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-3">
+        <ItemsDisplay items={data["items"]} />
+      </div>
+      <div className="row mt-3">
+        <SearchBar updateSearchParamas={updateFilters} />
+      </div>
+      <div className="row mt-3">
+        <AddItem addItem={addItemToData} />
+      </div>
     </div>
   );
 }
---
+++ src/ItemsDisplay.js
@@ -0,0 +1,36 @@
+function ItemsDisplay(props) {
+  const showItem = (item) => {
+    return (
+      <tr>
+        <th scope="row">{item.id} </th>
+        <td>{item.name}</td>
+        <td>{item.price}</td>
+        <td>{item.type}</td>
+        <td>{item.brand}</td>
+      </tr>
+    );
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h2>Items</h2>
+      </div>
+      <div className="row">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th className="col">Id</th>
+              <th className="col">Name</th>
+              <th className="col">Price</th>
+              <th className="col">Type</th>
+              <th className="col">Brand</th>
+            </tr>
+          </thead>
+          <tbody>{props.items.map(showItem)}</tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+export default ItemsDisplay;
